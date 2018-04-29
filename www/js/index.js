494adc6596da177dbb0c071021cfdf3bba407cee
Oscar Hakkinen <hako1_15@uni.worc.ac.uk>
2018-04-29 16:39:10 +0100

    marker 2

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 var watchID;
 var map;
 var loc;
-var loc2;
 var path = [];
 
 
@@ -21,18 +20,17 @@ $(document).on( "pageinit", "#pagethree", function() {
 
 function onLocationSuccess(position) {
     
-    loc = new google.maps.LatLng(52.191085, -2.225975);
-    loc2 = new google.maps.LatLng(52.196020, -2.225454);
+    loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    
+    position.coords.latitude, position.coords.longitude
     
     path.push(loc);
-    path.push(loc2);
     map = new google.maps.Map(document.getElementById('map'), {
         center: loc,
         zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     
-    
+    //sets a path that shows the location the users has travelled 
      var mapPath = new google.maps.Polyline({
           path: path,
           geodesic: true,
@@ -40,14 +38,14 @@ function onLocationSuccess(position) {
           strokeOpacity: 1.0,
           strokeWeight: 2
         });
-        
+        //creates a custom map marker
     var marker = new google.maps.Marker({
           map: map,
           draggable: false,
           animation: google.maps.Animation.DROP,
           position: {lat: 52.191085, lng: -2.225975}
         });
-    
+        //creates another custom map marker
      var marker2 = new google.maps.Marker({
           map: map,
           draggable: false,
